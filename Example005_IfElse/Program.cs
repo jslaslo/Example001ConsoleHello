d1254a00abec0e44aaf1e8a1a395b7a1756fa7c1
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2023-07-30 22:36:59 +0200

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите имя пользователя:");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "karina")
+{
+System.Console.WriteLine("Приветик, Карина)");
+}
+else
+{
+System.Console.WriteLine($"Привет, {userName}");
+}
