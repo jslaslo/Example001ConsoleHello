d435ea0c3c1781a0eb903af297528a4d40889806
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2023-07-30 22:56:07 +0200

    add Example 006 Base

---
+++ Example006_base/Program.cs
@@ -0,0 +1,33 @@
+﻿int [] arrayWeights = new int [5];
+AsignValueWeights(arrayWeights);
+PrintArrayWeights(arrayWeights);
+System.Console.WriteLine("");
+DetermineMaxWeight(arrayWeights);
+
+
+void AsignValueWeights(int [] arrayWeights)
+{
+    for(int i = 0; i < arrayWeights.Length; i++)
+    {
+        arrayWeights[i] = new Random().Next(1,20); 
+    }
+}
+void PrintArrayWeights(int [] arrayWeights)
+{
+    for(int i = 0; i < arrayWeights.Length; i++)
+    {
+        System.Console.Write(arrayWeights[i] + " ");
+    }
+}
+void DetermineMaxWeight(int [] arrayWeights)
+{
+    int max = arrayWeights[0];
+    for(int i = 1; i < arrayWeights.Length; i++)
+    {
+        if(arrayWeights[i] > max)
+        {
+            max = arrayWeights[i];
+        }          
+    }
+    System.Console.WriteLine(max);
+}
