ea647daf9099fd4c3ea9367b55c8ab29a58cd459
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2023-07-30 22:20:41 +0200

    add Example 004 Div

---
+++ Examole004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿double firstValue = new Random().Next(1,10);
+System.Console.WriteLine(firstValue);
+double secondValue = new Random().Next(1,10);
+System.Console.WriteLine(secondValue);
+System.Console.WriteLine(firstValue/secondValue);
